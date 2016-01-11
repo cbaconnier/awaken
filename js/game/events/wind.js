0c8b990f992940ad5d20a6062021a897037c23d0
cbaconnier <clement.baconnier@gmail.com>
2016-01-11 12:25:51 +0100

    Added wind to wind..

--- js/game/events/wind.js
+++ js/game/events/wind.js
@@ -9,18 +9,33 @@ Wind.prototype.constructor = Wind;
 
 
 Wind.prototype.create = function(){
+    this.windIsAvailable = false;
+    this.windForce = 0;
 
-
+    this.windTimer = this.game.time.create(false);
+    this.windTimer.start();
+    this.windTimer.add(Math.random() * (4000 - 2000)+2000, this.changeWind, this);
 };
 
 
+Wind.prototype.changeWind = function(){
+    if(this.windForce) {
+        this.windForce = 0;
+    }else{
+        this.windForce = 200;
+    }
+    this.windTimer.add(Math.random() * (4000 - 2000)+2000, this.changeWind, this);
+};
+
 Wind.prototype.update = function(snow, entities){
-    if(snow !== undefined) snow.changeWindDirection(200);
 
-    entities.forEachAlive(function(sprite){
-        sprite.body.x+=1;
-    });
+        if(snow !== undefined) snow.changeWindDirection(this.windForce);
 
+    if(this.windForce){
+        entities.forEachAlive(function(sprite){
+            sprite.body.x+=1;
+        });
+    }
 
 };
 
