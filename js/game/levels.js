4723b8a11457aaba0382eeb4fa89dfa53ff8473d
cbaconnier <clement.baconnier@gmail.com>
2016-01-29 09:53:48 +0100

    restored first level

--- js/game/levels.js
+++ js/game/levels.js
@@ -163,7 +163,7 @@
          */
 
         getFirstLevel: function () {
-            return this.getLevels().level_7;
+            return this.getLevels().level_1;
         },
 
         getGameOver: function(){
